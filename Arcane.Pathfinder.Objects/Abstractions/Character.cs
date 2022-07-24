f0b95467dd5f000fe467ecc38342f903170aed22
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-07-23 21:30:19 -0300

    Agregar archivos de proyecto.

---
+++ Arcane.Pathfinder.Objects/Abstractions/Character.cs
@@ -0,0 +1,8 @@
+﻿namespace Arcane.Pathfinder.Objects.Abstractions
+{
+    public abstract class Character
+    {
+        public string CharacterName { get; set; }
+        public string PlayerName { get; set; }
+    }
+}
