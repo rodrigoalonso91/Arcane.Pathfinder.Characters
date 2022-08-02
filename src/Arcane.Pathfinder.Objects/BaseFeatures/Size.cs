2b71a78abc5a4e8746bd065047d3765b55759cba
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-01 21:45:20 -0300

    Borra usings sin uso

--- src/Arcane.Pathfinder.Objects/BaseFeatures/Size.cs
+++ src/Arcane.Pathfinder.Objects/BaseFeatures/Size.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Arcane.Pathfinder.Objects.BaseFeatures
+﻿namespace Arcane.Pathfinder.Objects.BaseFeatures
 {
     public enum Size
     {
