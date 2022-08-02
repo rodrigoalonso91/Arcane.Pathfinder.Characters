f620d003ed6fd5d33e44567d5e67bb3dcd9f12f0
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-01 23:33:55 -0300

    Add SizeModifier class

---
+++ src/Arcane.Pathfinder.Objects/Utils/SizeModifier.cs
@@ -0,0 +1,16 @@
+﻿namespace Arcane.Pathfinder.Objects.Utils
+{
+    public class SizeModifier
+    {
+        // The special size modifier for both defense and attack
+        public const int Colossal = 8;
+        public const int Gargantuan = 4;
+        public const int Huge = 2;
+        public const int Large = 1;
+        public const int Medium = 0;
+        public const int Small = -1;
+        public const int Tiny = -2;
+        public const int Diminutive = -4;
+        public const int Fine = -8;
+    }
+}
