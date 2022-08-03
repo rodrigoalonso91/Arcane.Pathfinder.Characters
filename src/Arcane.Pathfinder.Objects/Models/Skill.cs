a5d7dfc2a70b3b06b6d3e17714a76321981738f6
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-03 01:18:32 -0300

    Create skill object and skill property

---
+++ src/Arcane.Pathfinder.Objects/Models/Skill.cs
@@ -0,0 +1,11 @@
+﻿namespace Arcane.Pathfinder.Objects.Models
+{
+    public class Skill
+    {
+        public string SkillName { get; set; } = string.Empty;
+        public bool IsClassSkill { get; set; }
+        public int Ranks { get; set; }
+        public bool HasArmorPenalty { get; set; }
+        // typemod
+    }
+}
