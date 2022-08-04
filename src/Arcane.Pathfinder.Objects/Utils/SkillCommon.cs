66282c86c3c57dc2eaa3b0fba2efcc4209980aba
Escojuan <escojuan16@gmail.com>
2022-08-03 21:48:52 -0300

    skills,atributos y baseattack

---
+++ src/Arcane.Pathfinder.Objects/Utils/SkillCommon.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Arcane.Pathfinder.Objects.Utils
+{
+     public enum SkillCommon
+    {
+        Acrobatics,
+        Appraise,
+        Bluff,
+        Climb,
+        Craft,
+        Diplomacy,
+        Disable_Device,
+        Disguise,
+        Escape_Artist,
+        Fly,
+        Handle_Animal,
+        Heal,
+        Intimidate,
+        Arcana,
+        Dungeoneering,
+        Geography,
+        History,
+        Local,
+        Nature,
+        Nobility,
+        Religion,
+        Linguistics,
+        Perception,
+        Perform,
+        Profession,
+        Ride,
+        Sense_Motive,
+        Sleight_of_Hand,
+        Spellcraft,
+        Stealth,
+        Survival,
+        Swim,
+        Use_Magic_Device,
+
+    }
+}
