72c1474f331e8697e66e91827e60d524bd177050
Escojuan <109829756+Escojuan@users.noreply.github.com>
2022-08-05 23:55:08 -0300

    Attributes (#21)
    * add
    
    * add
    
    * Delete IRace.cs
    
    Co-authored-by: Rodrigo Alonso <77740217+rodrigoalonso91@users.noreply.github.com>

--- src/Arcane.Pathfinder.Objects/Abstractions/Character.cs
+++ src/Arcane.Pathfinder.Objects/Abstractions/Character.cs
@@ -5,16 +5,9 @@ namespace Arcane.Pathfinder.Objects.Abstractions
 {
     public abstract class Character
     {
-        private int Strength = 10;
-        private int Dexterity = 10;
-        private int Constitution = 10;
-        private int Intelligence = 10;
-        private int Wisdom = 10;
-        private int Charisma = 10;
-
+        public CharacterAttributes? CharacterAttributes { get; set; }
         public string CharacterName { get; set; } = "Nameless";
         public string PlayerName { get; set; } = "Player";
-        public CharacterSize Size { get; set; }
         public int Level { get; private set; }
         public int Experience { get; private set; }
         public int HitDice { get; private set; }
