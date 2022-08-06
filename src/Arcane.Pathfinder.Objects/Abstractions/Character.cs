0a4e7f95f7a24a2f2feff8a071e999b55aae72ab
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-06 01:03:00 -0300

    Refactor: Now character is a normal class

--- src/Arcane.Pathfinder.Objects/Abstractions/Character.cs
+++ src/Arcane.Pathfinder.Objects/Abstractions/Character.cs
@@ -3,17 +3,16 @@ using Arcane.Pathfinder.Objects.Utils;
 
 namespace Arcane.Pathfinder.Objects.Abstractions
 {
-    public abstract class Character
+    public class Character
     {
-        public BasicAttributes? CharacterAttributes { get; set; }
         public string CharacterName { get; set; } = "Nameless";
         public string PlayerName { get; set; } = "Player";
         public int Level { get; private set; }
         public int Experience { get; private set; }
         public int HitDice { get; private set; }
         public int HeroPoints { get; set; }
+        public BasicAttributes? CharacterAttributes { get; set; }
+        public List<Skill>? CharacterSkills { get; set; }
         public Alignments Alignment { get; private set; }
-
-        public List<Skill> CharacterSkills = new();
     }
 }
