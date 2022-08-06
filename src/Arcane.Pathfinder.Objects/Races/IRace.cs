f4c28db9be9cd84cba50e296f05d7fda72f7f829
Escojuan <escojuan16@gmail.com>
2022-08-05 23:43:26 -0300

    add

---
+++ src/Arcane.Pathfinder.Objects/Races/IRace.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Arcane.Pathfinder.Objects.Utils;
+
+namespace Arcane.Pathfinder.Objects.Races
+{
+    internal class IRace
+    {
+        public CharacterSize Size { get; set; }
+    }
+}
