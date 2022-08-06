d1602266189a4e8472116db9429f8e720b455950
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-06 00:24:05 -0300

    Add Race abstract class

--- src/Arcane.Pathfinder.Objects/Races/Elf.cs
+++ src/Arcane.Pathfinder.Objects/Races/Elf.cs
@@ -1,6 +1,23 @@
-﻿namespace Arcane.Pathfinder.Objects.Races
+﻿using Arcane.Pathfinder.Objects.Models;
+using Arcane.Pathfinder.Objects.Utils;
+
+namespace Arcane.Pathfinder.Objects.Races
 {
-    public class Elf
+    public class Elf : Race
     {
+        public Elf(int movement)
+        {
+            Languages = new List<string>() { "Common", "Elf" };
+            Movement = movement;
+            Vision = VisionType.LowLight;
+            Size = CharacterSize.Medium;
+
+            RaceAtributtes = new CharacterAttributes
+            {
+                Dexterity = 2,
+                Intelligence = 2,
+                Constitution = -2
+            };
+        }
     }
 }
---
+++ src/Arcane.Pathfinder.Objects/Races/Race.cs
@@ -0,0 +1,14 @@
+﻿using Arcane.Pathfinder.Objects.Models;
+using Arcane.Pathfinder.Objects.Utils;
+
+namespace Arcane.Pathfinder.Objects.Races
+{
+    public abstract class Race
+    {
+        public List<string>? Languages;
+        public CharacterAttributes? RaceAtributtes { get; set; }
+        public CharacterSize Size { get; set; }
+        public VisionType Vision { get; set; }
+        public int Movement { get; set; }
+    }
+}
