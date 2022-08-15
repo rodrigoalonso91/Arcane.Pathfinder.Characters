8adca8c774f01356887a85eb07c67798cb465204
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-15 15:39:59 -0300

    Add Human constructor

--- src/Arcane.Pathfinder.Characters/Races/Human.cs
+++ src/Arcane.Pathfinder.Characters/Races/Human.cs
@@ -1,6 +1,15 @@
-﻿namespace Arcane.Pathfinder.Characters.Races
+﻿using Arcane.Pathfinder.Characters.Utils;
+
+namespace Arcane.Pathfinder.Characters.Races
 {
-    public class Human
+    public class Human : Race
     {
+        public Human(int movement)
+        {
+            Languages = new List<string>() { "Common" };
+            Movement = movement;
+            Vision = VisionType.Normal;
+            Size = CharacterSize.Medium;
+        }
     }
 }
