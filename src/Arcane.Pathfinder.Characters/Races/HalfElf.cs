428803c152d68f9c80e39a06e04d02cf7c6182e6
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-08-22 21:49:46 -0300

    Add HalfOrc, Halfling, Gnome, HalfElf and Dwarf classes

---
+++ src/Arcane.Pathfinder.Characters/Races/HalfElf.cs
@@ -0,0 +1,15 @@
+﻿using Arcane.Pathfinder.Characters.Utils;
+
+namespace Arcane.Pathfinder.Characters.Races
+{
+    public class HalfElf : Race
+    {
+        public HalfElf(int movement)
+        {
+            Languages = new List<string>() { "Common", "Elf" };
+            Movement = movement;
+            Vision = VisionType.Normal;
+            Size = CharacterSize.Medium;
+        }
+    }
+}
