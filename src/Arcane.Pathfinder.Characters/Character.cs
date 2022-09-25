85eae9440a72b49ee065e4740c61ab26781a80d7
Rodrigo Alonso <rodrigoalonso.dev@gmail.com>
2022-09-25 13:16:29 -0300

    Add initiative and Basic attack properties

--- src/Arcane.Pathfinder.Characters/Character.cs
+++ src/Arcane.Pathfinder.Characters/Character.cs
@@ -8,12 +8,18 @@ namespace Arcane.Pathfinder.Characters
     {
         public string CharacterName { get; set; } = "Nameless";
         public string PlayerName { get; set; } = "Player";
-        public int Level { get; private set; }
-        public int Experience { get; private set; }
+        public int Level { get; private set; } = 1;
+        public int Experience { get; private set; } = 0;
         public int HitDice { get; private set; }
         public int HeroPoints { get; set; }
-        public BasicAttributes? CharacterAttributes { get; set; }
+        public int BasicAttack { get; set; }
+        public BasicAttributes CharacterAttributes { get; set; } = new();
         public List<Skill>? CharacterSkills { get; set; }
         public Alignments Alignment { get; private set; }
+
+        public int GetInitiativeMod(int otherMods = 0)
+        {
+            return CharacterAttributes.Dexterity + otherMods;
+        }
     }
 }
